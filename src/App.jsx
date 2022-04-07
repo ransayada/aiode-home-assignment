f38bbdc864f29c4ec8d907d849e9294323c77c12
ransayada <ran.sayada@gmail.com>
2022-04-07 14:54:54 +0300

    deploy + favicon + title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './index.css';
+import "./index.css";
 import { useState, useRef, useEffect } from "react";
-import track from './assets/audio/track.wav';
-import trackRev from './assets/audio/track-reverse.wav';
+import track from "./assets/audio/track.wav";
+import trackRev from "./assets/audio/track-reverse.wav";
 
 //handle key press hook
 const useKeyPress = (targetKey) => {
@@ -28,12 +28,11 @@ const useKeyPress = (targetKey) => {
   return keyPressed;
 };
 
-
 const App = () => {
-  const speeds = [5.0, 4.0, 3.0, 2.0, 1.0,0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
+  const speeds = [5.0, 4.0, 3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
   const [isReverse, setIsReverse] = useState(false);
-  const [isPlaying,setIsPlaying] = useState(false);
-  const [playBackSpeedIndex,setPlayBackSpeedIndex] = useState(6);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playBackSpeedIndex, setPlayBackSpeedIndex] = useState(6);
   const audioRegRef = useRef();
   const audioRevRef = useRef();
   //key press hooks
@@ -41,75 +40,84 @@ const App = () => {
   const lPressed = useKeyPress("l");
   // const kPressed = useKeyPress("k"); //there is no use for the assignment but there is use in the future
   const escapePressed = useKeyPress("Escape");
-  
-  //Click on K button or K-Key pressed
-  const play =() =>{
-      if(isPlaying){
-        setPlayBackSpeedIndex(6);
-        audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
-        setIsReverse(false);
-        audioRegRef.current.pause();
-        audioRevRef.current.pause();
-      }else{
-          audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
-          audioRegRef.current.play();
-          audioRevRef.current.play();
-        }
-      setIsPlaying(!isPlaying);
-    };
-  //Click on L button or L-Key pressed
-  const speedUp = () => {
-      if(playBackSpeedIndex < speeds.length-1){
-        if(playBackSpeedIndex<5){setIsReverse(true);}
-        if(playBackSpeedIndex>5){setIsReverse(false);}
-        setPlayBackSpeedIndex(playBackSpeedIndex+1);
-        audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
-        audioRevRef.current.playbackRate = speeds[playBackSpeedIndex];
-      }
-    };
-  //Click on J button or J-Key pressed
-  const speedDown = () => {
-      if(playBackSpeedIndex > 0){
-        if(playBackSpeedIndex<5){setIsReverse(true);}
-        if(playBackSpeedIndex>5){setIsReverse(false);}
-        setPlayBackSpeedIndex(playBackSpeedIndex-1);
-        audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
-        audioRevRef.current.playbackRate = speeds[playBackSpeedIndex];
-      }   
-    };
 
-    const terminate= () =>{
-      alert('Program Terminated');
-    }
-    if(escapePressed){
+  //Click on K button or K-Key pressed
+  const play = () => {
+    if (isPlaying) {
+      setPlayBackSpeedIndex(6);
+      audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
+      setIsReverse(false);
       audioRegRef.current.pause();
       audioRevRef.current.pause();
+    } else {
+      audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
+      audioRegRef.current.play();
+      audioRevRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+  //Click on L button or L-Key pressed
+  const speedUp = () => {
+    if (playBackSpeedIndex < speeds.length - 1) {
+      if (playBackSpeedIndex < 5) {
+        setIsReverse(true);
+      }
+      if (playBackSpeedIndex > 5) {
+        setIsReverse(false);
+      }
+      setPlayBackSpeedIndex(playBackSpeedIndex + 1);
+      audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
+      audioRevRef.current.playbackRate = speeds[playBackSpeedIndex];
+    }
+  };
+  //Click on J button or J-Key pressed
+  const speedDown = () => {
+    if (playBackSpeedIndex > 0) {
+      if (playBackSpeedIndex < 5) {
+        setIsReverse(true);
+      }
+      if (playBackSpeedIndex > 5) {
+        setIsReverse(false);
+      }
+      setPlayBackSpeedIndex(playBackSpeedIndex - 1);
+      audioRegRef.current.playbackRate = speeds[playBackSpeedIndex];
+      audioRevRef.current.playbackRate = speeds[playBackSpeedIndex];
+    }
+  };
+
+  const terminate = () => {
+    alert("Program Terminated");
+  };
+  if (escapePressed) {
+    audioRegRef.current.pause();
+    audioRevRef.current.pause();
+    terminate();
+  }
+
+  //Key press handler
+  const handleKeyPress = (ev) => {
+    const key = ev.key;
+    if (key === "k") {
+      play();
+    } else if (key === "j") {
+      speedDown();
+    } else if (key === "l") {
+      speedUp();
+    } else if (key === "Escape") {
       terminate();
     }
-    
+  };
 
-    //Key press handler
-    const handleKeyPress = (ev) => {
-      const key = ev.key;
-      if (key === "k") {
-        play();
-      } else if (key === "j") {
-        speedDown();
-      } else if (key === "l") {
-        speedUp();
-      } else if (key === "Escape") {
-        terminate();
-      }
-    };
-
-    //DOM
+  //DOM
   return (
     <div className="App">
       <div className="title">
-        <h1>a<span>I</span>o<span>D</span>e</h1>
+        <h1>
+          a<span>I</span>o<span>D</span>e
+        </h1>
       </div>
       <div className="audio">
-      <audio
+        <audio
           loop
           muted={isReverse}
           ref={audioRegRef}
@@ -125,8 +133,8 @@ const App = () => {
         ></audio>
       </div>
       <div className="buttons">
-      <div className="btn-container">
-      <button
+        <div className="btn-container">
+          <button
             className={!jPressed ? "btn-not-active" : "btn-active"}
             onClick={speedDown}
             onKeyPress={handleKeyPress}
@@ -134,13 +142,12 @@ const App = () => {
             J
           </button>
           <button
-            
             className={!isPlaying ? "btn-not-active" : "btn-pressed"}
             onClick={play}
             onKeyPress={handleKeyPress}
           >
             K
-            </button>
+          </button>
           <button
             className={!lPressed ? "btn-not-active" : "btn-active"}
             onClick={speedUp}
@@ -148,11 +155,10 @@ const App = () => {
           >
             L
           </button>
-      </div>
+        </div>
       </div>
     </div>
   );
-}
-
+};
 
 export default App;
